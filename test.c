aa48ab48cb15cef25c2bec69058b73e31ecd8438
Stan le Punk <stanislasiii@gmail.com>
2020-09-29 13:27:15 +0200

    Update test.c

--- test.c
+++ test.c
@@ -5,52 +5,44 @@
 #include<assert.h> 
   
 /* Link list node */
-struct Node 
-{ 
-    int data; 
-    struct Node* next; 
+struct Node { 
+  int data; 
+  struct Node* next; 
 }; 
   
 /* Function to delete the entire linked list */
-void deleteList(struct Node** head_ref) 
-{ 
-   /* deref head_ref to get the real head */
-   struct Node* current = *head_ref; 
-   struct Node* next; 
+void deleteList(struct Node** head_ref) { 
+  /* deref head_ref to get the real head */
+  struct Node* current = *head_ref; 
+  struct Node* next; 
   
-   while (current != NULL)  
-   { 
-       next = current->next; 
-       free(current); 
-       current = next; 
-   } 
+  while (current != NULL) {
+    next = current->next; 
+    free(current); 
+    current = next; 
+  } 
     
    /* deref head_ref to affect the real head back 
       in the caller. */
-   *head_ref = NULL; 
+  *head_ref = NULL; 
 } 
   
 /* Given a reference (pointer to pointer) to the head 
   of a list and an int, push a new node on the front 
   of the list. */
-void push(struct Node** head_ref, int new_data) 
-{ 
+void push(struct Node** head_ref, int new_data) { 
     /* allocate node */
     struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
-  
     /* put in the data  */
-    new_node->data  = new_data; 
-     
+    new_node->data = new_data;    
     /* link the old list off the new node */
     new_node->next = (*head_ref); 
-     
     /* move the head to point to the new node */
-    (*head_ref)    = new_node; 
+    (*head_ref) = new_node; 
 } 
   
 /* Driver program to test count function*/
-int main() 
-{ 
+int main() { 
     /* Start with the empty list */
     struct Node* head = NULL; 
      
